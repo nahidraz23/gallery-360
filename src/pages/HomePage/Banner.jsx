9ffc678c04106ef6f6d48292a168abe657cc39ac
nahidraz23 <nzamanraz@gmail.com>
2024-11-18 20:55:57 +0600

    banner section updated

--- src/pages/HomePage/Banner.jsx
+++ src/pages/HomePage/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
         <div
             className="hero h-[863px] lg:h-[929px]"
             style={{
-                backgroundImage: "url(https://images.unsplash.com/photo-1665686377065-08ba896d16fd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8b25saW5lJTIwc2hvcHBpbmd8ZW58MHwwfDB8fHww)",
+                backgroundImage: "url(https://cdn.vectorstock.com/i/500p/91/98/shopping-online-with-discount-conceptual-banner-vector-47519198.jpg)",
             }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-neutral-content text-center">
